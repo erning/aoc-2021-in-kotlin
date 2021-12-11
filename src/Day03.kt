ac0071a571385e193475f786da7b3dea48e1bea9
Zhang Erning <zhang@erning.com>
2021-12-11 23:54:00 +0800

    day 3

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,43 +1,70 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        val rows = input.size
         val cols = input[0].length
-        val grid = Array(cols) { IntArray(rows) }
-        for (y in 0 until rows) {
-            for (x in 0 until cols) {
-                grid[x][y] = input[y][x].digitToInt()
+        val grid = (0 until cols).map { i ->
+            input.map {
+                it[i]
             }
         }
-        val rate = grid.map {
-            it.count { v -> v == 1 }
+        val gamma = grid.map {
+            it.sumOf { c ->
+                c.digitToInt() * 2 - 1
+            }
         }.map {
-            if (it > rows / 2) Pair(1, 0) else Pair(0, 1)
-        }.reduce { acc, s ->
-            Pair(
-                acc.first.shl(1).or(s.first),
-                acc.second.shl(1).or(s.second),
-            )
+            if (it >= 0) 1 else 0
         }
-        return rate.first * rate.second
+        val epsilon = gamma.map {
+            if (it == 0) 1 else 0
+        }
+        val gammaRate = gamma.reduce { acc, v ->
+            acc.shl(1).or(v)
+        }
+        val epsilonRate = epsilon.reduce { acc, v ->
+            acc.shl(1).or(v)
+        }
+
+        return gammaRate * epsilonRate
     }
 
     fun part2(input: List<String>): Int {
-        val rows = input.size
         val cols = input[0].length
-        val grid = Array(cols) { IntArray(rows) }
-        for (y in 0 until rows) {
-            for (x in 0 until cols) {
-                grid[x][y] = input[y][x].digitToInt()
+        fun rating(a: Int, b: Int): Int {
+            var grid = input.toMutableList()
+            for (i in 0 until cols){
+                val v = grid.map {
+                    it[i]
+                }.sumOf { c ->
+                    c.digitToInt() * 2 - 1
+                }
+                val bit = when {
+                    v > 0 -> a
+                    v < 0 -> b
+                    else -> a
+                }
+                grid = grid.filter {
+                    it[i].digitToInt() == bit
+                }.toMutableList()
+                if (grid.size <= 1) {
+                    break
+                }
+            }
+            return grid.first().map {
+                it.digitToInt()
+            }.reduce { acc, v ->
+                acc.shl(1).or(v)
             }
         }
-        return 0
+
+        val oxygenRating = rating(1, 0)
+        val co2Rating = rating(0, 1)
+        return oxygenRating * co2Rating
     }
 
     val day = 3
 
     val exampleInput = readExample(day)
     check(part1(exampleInput) == 198)
-//    check(part2(exampleInput) == 230)
+    check(part2(exampleInput) == 230)
 
     val input = readInput(day)
     println(part1(input))
