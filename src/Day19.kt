133df6f891f0d81a5faf265d8f0ada662abc2df2
Zhang Erning <zhang@erning.com>
2021-12-20 13:19:12 +0800

    day 19

--- src/Day19.kt
+++ src/Day19.kt
@@ -1,68 +1,113 @@
+import kotlin.math.abs
+
 fun main() {
     data class Point(val x: Int, val y: Int, val z: Int) {
-        fun rotate(rotation: Int): Point {
-            return when (rotation) {
+        operator fun plus(p: Point): Point {
+            return Point(x + p.x, y + p.y, z + p.z)
+        }
+
+        operator fun minus(p: Point): Point {
+            return Point(x - p.x, y - p.y, z - p.z)
+        }
+
+        operator fun unaryMinus(): Point {
+            return Point(-x, -y, -z)
+        }
+
+        fun rotate(orientation: Int): Point {
+            return when (orientation) {
                 0 -> Point(x, y, z)
                 1 -> Point(x, -z, y)
                 2 -> Point(x, -y, -z)
                 3 -> Point(x, z, -y)
 
-                4 -> Point(-x, y, z)
-                5 -> Point(-x, -z, y)
-                6 -> Point(-x, -y, -z)
-                7 -> Point(-x, z, -y)
+                4 -> Point(-x, y, -z)
+                5 -> Point(-x, -z, -y)
+                6 -> Point(-x, -y, z)
+                7 -> Point(-x, z, y)
 
-                8 -> Point(y, x, z)
-                9 -> Point(y, -z, x)
-                10 -> Point(y, -x, -z)
-                11 -> Point(y, z, -x)
+                8 -> Point(y, x, -z)
+                9 -> Point(y, -z, -x)
+                10 -> Point(y, -x, z)
+                11 -> Point(y, z, x)
 
                 12 -> Point(-y, x, z)
                 13 -> Point(-y, -z, x)
                 14 -> Point(-y, -x, -z)
                 15 -> Point(-y, z, -x)
 
-                16 -> Point(z, y, x)
-                17 -> Point(z, -x, y)
-                18 -> Point(z, -y, -x)
-                19 -> Point(z, x, -y)
+                16 -> Point(z, y, -x)
+                17 -> Point(z, x, y)
+                18 -> Point(z, -y, x)
+                19 -> Point(z, -x, -y)
 
                 20 -> Point(-z, y, x)
                 21 -> Point(-z, -x, y)
                 22 -> Point(-z, -y, -x)
                 23 -> Point(-z, x, -y)
 
-                else -> Point(x, y, z)
+                else -> error("invalid")
             }
         }
 
-        fun distanceSquare(other: Point): Int {
-            val dx = other.x - x
-            val dy = other.y - y
-            val dz = other.z - z
-            return dx * dx + dy * dy + dz * dz
+        fun manhattanDistance(other: Point): Int {
+            val p = other - this
+            return abs(p.x) + abs(p.y) + abs(p.z)
         }
     }
 
-    class Scanner(val beacons: List<Point>) {
-        var _position: Point = Point(0, 0, 0)
+    data class Scanner(val position: Point, val beacons: Set<Point>) {
+        constructor(beacons: Set<Point>) : this(Point(0, 0, 0), beacons)
+        constructor(beacons: List<Point>) : this(Point(0, 0, 0), beacons.toSet())
+        constructor(position: Point, beacons: List<Point>) : this(position, beacons.toSet())
 
-        fun getBeacons(rotation: Int = 0): List<Point> {
-            return beacons.map {
-                it.rotate(rotation)
+        override fun toString(): String {
+            var s = "(${position.x}, ${position.y}, ${position.z})\n"
+            for (beacon in beacons) {
+                s += "${beacon.x}, ${beacon.y}, ${beacon.z}\n"
             }
+            return s
         }
-    }
 
-    fun parsePoint(s: String): Point {
-        return s.split(",")
-            .map { it.toInt() }
-            .let { Point(it[0], it[1], it[2]) }
+        fun move(newPosition: Point): Scanner {
+            val delta = newPosition - position
+            return Scanner(newPosition, beacons.map { it + delta })
+        }
+
+        fun rotate(orientation: Int): Scanner {
+            return Scanner(beacons.map { it.rotate(orientation) })
+        }
+
+        fun intersect(other: Scanner): Int {
+            return beacons.intersect(other.beacons).size
+        }
+
+        fun match(other: Scanner): Scanner? {
+            for (beacon in beacons) {
+                for (orientation in 0..23) {
+                    val rotated = other.rotate(orientation)
+                    for (otherBeacon in rotated.beacons) {
+                        val p = position + beacon - otherBeacon
+                        val scanner = rotated.move(p)
+                        if (intersect(scanner) >= 12) {
+                            return scanner
+                        }
+                    }
+                }
+            }
+            return null
+        }
     }
 
     fun parseInput(input: List<String>): List<Scanner> {
         val scanners: MutableList<Scanner> = mutableListOf()
         var i = 0
+
+        fun parsePoint(s: String): Point {
+            return s.split(",")
+                .map { it.toInt() }
+                .let { Point(it[0], it[1], it[2]) }
+        }
 
         fun parseBeacons(): List<Point> {
             val beacons: MutableList<Point> = mutableListOf()
@@ -86,11 +131,50 @@ fun main() {
 
     fun part1(input: List<String>): Int {
         val scanners = parseInput(input)
-        return 0
+        var base = scanners.first()
+        var remaining: MutableList<Scanner?> = scanners.drop(1).toMutableList()
+
+        while (remaining.isNotEmpty()) {
+            for (i in 0 until remaining.size) {
+                val other = remaining[i] ?: continue
+                val scanner = base.match(other) ?: continue
+                base = Scanner(base.beacons.union(scanner.beacons))
+                remaining[i] = null
+            }
+            remaining = remaining.filterNotNull().toMutableList()
+        }
+        return base.beacons.size
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val scanners = parseInput(input)
+        var base = scanners.first()
+        var remaining: MutableList<Scanner?> = scanners.drop(1).toMutableList()
+
+        val positions: MutableList<Point> = mutableListOf(base.position)
+
+        while (remaining.isNotEmpty()) {
+            for (i in 0 until remaining.size) {
+                val other = remaining[i] ?: continue
+                val scanner = base.match(other) ?: continue
+                base = Scanner(base.beacons.union(scanner.beacons))
+                remaining[i] = null
+                positions.add(scanner.position)
+                println(base.beacons.size)
+            }
+            remaining = remaining.filterNotNull().toMutableList()
+        }
+
+        var md = Int.MIN_VALUE
+        for (p1 in positions) {
+            for (p2 in positions) {
+                val d = p1.manhattanDistance(p2)
+                if (d > md) {
+                    md = d
+                }
+            }
+        }
+        return md
     }
 
     val day = 19
@@ -104,6 +188,6 @@ fun main() {
     check(part1(exampleInput) == 79)
     print("Part One: "); println(part1(input))
 
-    check(part2(exampleInput) == 0)
+    check(part2(exampleInput) == 3621)
     print("Part Two: "); println(part2(input))
 }
