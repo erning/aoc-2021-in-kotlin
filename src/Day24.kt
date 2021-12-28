860bc5936aacb8570a3e5ffd168b8804adbd4152
Zhang Erning <zhang@erning.com>
2021-12-28 10:33:27 +0800

    day 24

--- src/Day24.kt
+++ src/Day24.kt
@@ -1,121 +1,59 @@
-import java.io.Reader
-import java.io.StringReader
-import java.nio.CharBuffer
-
 fun main() {
-    val registers = setOf("w", "x", "y", "z")
+    fun search(program: List<String>, digits: List<Int>): String? {
+        val visited: MutableSet<Pair<Int, Map<Char, Long>>> = mutableSetOf()
+        var final: String? = null
 
-    class ALU {
-        lateinit var input: Reader
-        val variables: MutableMap<String, Long> = mutableMapOf()
+        fun exec(pc: Int, regs: Map<Char, Long>, ds: List<Int>): Boolean {
+            if (visited.contains(pc to regs)) return false
+            visited.add(pc to regs)
+            if ((regs['z'] ?: 0) > 1000000L) return false
+            
+            if (pc >= program.size) {
+                if (regs['z'] == 0L) {
+                    final = ds.joinToString("") { it.toString() }
+                    return true
+                }
+                return false
+            }
 
-        override fun toString(): String {
-            return "ALU: w=${variables["w"] ?: 0} x=${variables["x"] ?: 0} y=${variables["y"] ?: 0} z=${variables["z"] ?: 0}"
+            val rv = regs.toMutableMap()
+            val inst = program[pc]
+            val op = inst.substring(0, 3)
+            val a = inst.substring(4, 5)
+            if (op == "inp") {
+                for (d in digits) {
+                    rv[a[0]] = d.toLong()
+                    if (exec(pc + 1, rv, ds + d)) return true
+                }
+                return false
+            }
+            val b = inst.substring(6)
+            val av = regs[a[0]] ?: 0
+            val bv: Long = b.toLongOrNull() ?: (regs[b[0]] ?: 0)
+            rv[a[0]] = when (op) {
+                "add" -> av + bv
+                "mul" -> av * bv
+                "div" -> av / bv
+                "mod" -> av % bv
+                "eql" -> if (av == bv) 1 else 0
+                else -> error("unknown operator")
+            }
+            return exec(pc + 1, rv, ds)
         }
-
-        operator fun set(a: String, b: String) {
-            set(a, if (b in registers) get(b) else b.toLong())
-        }
-
-        operator fun set(a: String, b: Long) {
-            assert(a.length == 1)
-            assert(a in registers)
-            variables[a] = b
-        }
-
-        operator fun get(a: String): Long {
-            return a.toLongOrNull() ?: variables[a] ?: 0L
-        }
-
-        val instructions: Map<String, (String, String) -> Unit> = mutableMapOf(
-            "inp" to { a, _ -> set(a, input.read().toLong()) },
-            "add" to { a, b -> set(a, get(a) + get(b)) },
-            "mul" to { a, b -> set(a, get(a) * get(b)) },
-            "div" to { a, b -> set(a, get(a) / get(b)) },
-            "mod" to { a, b -> set(a, get(a) % get(b)) },
-            "eql" to { a, b -> set(a, if (get(a) == get(b)) 1 else 0) }
-        )
-
-        fun execute(instruction: String) {
-            val args = instruction.split(" ")
-            val op = args[0]
-            val a = args[1]
-            val b = if (args.size >= 3) args[2] else ""
-            val f = instructions[op] ?: return
-            f(a, b)
-        }
-
-        fun reset() {
-            variables.clear()
-        }
+        exec(0, mapOf(), listOf())
+        return final
     }
 
-    class DigitReader(s: String) : StringReader(s) {
-        override fun read(): Int {
-            return super.read() - 48
-        }
+    fun part1(input: List<String>): String? {
+        return search(input, (1..9).reversed().toList())
     }
 
-
-    fun runProgram(input: String, instructions: List<String>): ALU {
-        val alu = ALU()
-        alu.input = DigitReader(input)
-        instructions.forEach { alu.execute(it) }
-        return alu
-    }
-
-    fun testALU1() {
-        val program = """
-            inp x
-            mul x -1
-        """.trimIndent().split("\n")
-        val alu = runProgram("1", program)
-        println(alu)
-    }
-
-    fun testALU2() {
-        val program = """
-            inp z
-            inp x
-            mul z 3
-            eql z x
-        """.trimIndent().split("\n")
-        val alu = runProgram("13", program)
-        println(alu)
-    }
-
-    fun testALU3() {
-        val program = """
-            inp w
-            add z w
-            mod z 2
-            div w 2
-            add y w
-            mod y 2
-            div w 2
-            add x w
-            mod x 2
-            div w 2
-            mod w 2
-        """.trimIndent().split("\n")
-        val alu = runProgram("7", program)
-        println(alu)
-    }
-
-    fun part1(input: List<String>): Int {
-        return 0
-    }
-
-    fun part2(input: List<String>): Int {
-        return 0
+    fun part2(input: List<String>): String? {
+        return search(input, (1..9).toList())
     }
 
     val day = 24
     val title = "Arithmetic Logic Unit"
-
-    testALU1()
-    testALU2()
-    testALU3()
 
     val input = readInput(day)
 
